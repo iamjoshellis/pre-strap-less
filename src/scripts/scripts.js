9ec69e7da392df68f5e71136f6b101fe6d179392
Josh Ellis <Josh@Joshs-iMac.local>
2015-07-23 01:08:09 +0100

    First Commit!
    A small step for Scott, but a great leap for his front-end skillz!

---
+++ src/scripts/scripts.js
@@ -0,0 +1,15 @@
+//Don't write js here, just call other .js files. This syntax is unique to Prepros, but you could use any build tool to compile js. jQuery is called using CDN, see index.html as an example.
+
+// @prepros-prepend "bootstrap/affix.js"
+// @prepros-prepend "bootstrap/transition.js"
+// @prepros-prepend "bootstrap/tooltip.js"
+// @prepros-prepend "bootstrap/alert.js"
+// @prepros-prepend "bootstrap/button.js"
+// @prepros-prepend "bootstrap/carousel.js"
+// @prepros-prepend "bootstrap/collapse.js"
+// @prepros-prepend "bootstrap/dropdown.js"
+// @prepros-prepend "bootstrap/modal.js"
+// @prepros-prepend "bootstrap/popover.js"
+// @prepros-prepend "bootstrap/scrollspy.js"
+// @prepros-prepend "bootstrap/tab.js"
+// @prepros-prepend "main.js"
